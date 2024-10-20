6836a9a7b6dd98be642ddda7f2638ea1287a7ca1
Khoa Phạm <pnmkhoa1234@gmail.com>
2024-10-21 00:02:34 +0700

    Upload BTH2

---
+++ BTH2_TheStore/BTH2_TheStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BTH2_TheStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
